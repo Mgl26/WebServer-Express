074aee395922ee6d809fa4c857b8ed688a64373d
Miguel <mjsoto@entel.cl>
2018-07-20 17:14:39 -0400

    primera carga de mi app

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+
+require('./hbs/helpers');
+
+app.use(express.static(__dirname + '/public'));
+
+//Express HBS engine
+hbs.registerPartials(__dirname + '/views/parciales');
+app.set('view engine', 'hbs');
+
+
+app.get('/', (req, res) => {
+
+    res.render('home', {
+        nombre: 'Miguel soto'
+    });
+
+});
+
+app.get('/about', (req, res) => {
+
+    res.render('about');
+
+});
+app.listen(3000, () => {
+    console.log('Escuchando peticiones con el puerto 3000');
+})
